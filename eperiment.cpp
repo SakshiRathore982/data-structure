5d66c577dcbbffe5a7dced4179545f0948b3bc63
sakshi rathore <sakshi.17jics104@jietjodhpur.ac.in>
2021-03-16 23:44:56 +0530

    Add files via upload

---
+++ eperiment.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+	ifstream fin;
+	ofstream fout;
+	char ch;
+	int key;
+	cout<<"enter the value of key in numberic:";
+	cin>>key;
+	fin.open("input.c");
+	fout.open("output.c");
+	ch=fin.get();
+	cout<<"cipher text is ";
+	while(!fin.eof())
+	{
+	 	if(isupper(ch))
+		    ch=(ch-65+key)% 26 + 65;
+		else
+	    	ch=(ch-97+key)% 26 + 97;
+		cout<<ch;
+		fout.put(ch);
+		ch=fin.get();
+	}
+	fout.close();
+	return 0;
+	}
